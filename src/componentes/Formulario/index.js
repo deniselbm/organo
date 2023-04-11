b78ef588b08287f20aa8be928a664d611528c4a7
deniselbm <dlbmoreiratj@gmail.com>
2023-04-11 19:22:47 -0300

    novo

---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import Botao from '../Botao'
+import CampoTexto from '../CampoTexto/CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import './Formulario.css'
+
+
+const Formulario = (props) => {
+
+
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')
+    }
+    return (
+        <section className="formulario">
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar os cards</h2>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite sua série favorita" valor={nome} aoAlterado={valor => setNome(valor)} />
+
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" valor={imagem} aoAlterado={valor => setImagem(valor)} />
+                <ListaSuspensa obrigatorio={true} label="Categoria" itens={props.times} valor={time} aoAlterado={valor => setTime(valor)} />
+                <Botao>
+                    Criar Card
+                </Botao>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
